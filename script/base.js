2381579ff55d577de31720cc11f8546e27f6d82a
Dayse Campos <111383784+dayseCampos@users.noreply.github.com>
2022-10-20 22:01:29 -0300

    correções

--- script/base.js
+++ script/base.js
@@ -6,12 +6,13 @@ var tipo = document.querySelector(".tipo");
 var chave = document.getElementById("chave").value;
 var entrada = document.querySelector("#entrada");
 
+//metodo muda a cor se não for selecionado
 passo.style.display = "none";
 metodo.addEventListener("change", function () {
   metodo.style.backgroundColor = "#009eff";
   metodo.style.transition = "none";
   if (metodo.selectedIndex == 1) {
-    // "passo" aparece se Cifra for selecionado
+    //"passo" aparece se Cifra for selecionado
     passo.style.display = "flex";
   } else {
     passo.style.display = "none";
@@ -19,7 +20,7 @@ metodo.addEventListener("change", function () {
 });
 
 tipo.addEventListener("change", function () {
-  // muda msg do botão de enviar, conforme opção selecionada
+  //muda a mensagem do botão enviar conforme selecionado
   if (tipoRadio[0].checked) {
     enviar.textContent = "Codificar";
   } else {
@@ -28,7 +29,7 @@ tipo.addEventListener("change", function () {
 });
 
 passo.addEventListener("change", function () {
-  // atualiza o valor da chave
+  // atualiza o valor da chave de cesar
   chave = document.getElementById("chave").value;
 });
 
